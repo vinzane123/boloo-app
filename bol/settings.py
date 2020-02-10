a3a00822496a3025786a98fd5e57877afd3af780
vinodhp <vinodhpolamarasetty@gmail.com>
2020-02-10 16:17:54 +0530

    heroku-changes

--- bol/settings.py
+++ bol/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +140,6 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Kolkata'
 CELERY_RESULT_BACKEND='amqp://'
+
+
+django_heroku.settings(locals())
